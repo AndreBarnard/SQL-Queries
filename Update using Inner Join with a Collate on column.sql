dee1ae44fdb45cc392ea8ba868a49eb1c24360db
Andre Barnard <andrebarn@gmail.com>
2017-02-01 11:55:00 +0200

    Add files via upload

---
+++ Update using Inner Join with a Collate on column.sql
@@ -0,0 +1,8 @@
+
+BEGIN TRAN
+Update T2 Set T2.DefaultValue = T1.DefaultValue  from [DATABASE1].[dbo].[LocalisationKeys]  T1
+ inner join [DATABASE12].[dbo].[LocalisationKeys] T2
+ ON (T1.[Key] COLLATE Latin1_General_CI_AS  = T2.[Key])
+
+ --Commit
+ --RollBack
